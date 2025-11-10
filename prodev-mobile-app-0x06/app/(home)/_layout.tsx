41a4a60f796aa942f5a7acaf7b04ca9f14eca7d6
wamamili <wamamiliclean@gmail.com>
2025-11-10 10:29:56 +0300

    commit

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -1,38 +1,59 @@
-import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { Ionicons, Feather } from "@expo/vector-icons";
 
-
-const HomeRootLayout = () => {
+export default function Layout() {
   return (
-    <Tabs screenOptions={{
-        tabBarActiveTintColor: '#34967C',
-        
-    }} >
-      <Tabs.Screen name="index" options={{
-        title: 'Home',
-        tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />,
-      }} />
-      <Tabs.Screen name="search" options={{
-        title: 'Search',
-        headerShown: true,
-        tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
-      }} />
-      <Tabs.Screen name="saved" options={{
-        title: 'Saved',
-        headerShown: true,
-        tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />
-      }} />
-        <Tabs.Screen name="inbox" options={{
-        title: 'Inbox',
-        headerShown: true,
-        tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
-      }} />
-        <Tabs.Screen name="profile" options={{
-        title: 'Profile',
-        tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
-      }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#34967C",
+        tabBarInactiveTintColor: "#999",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubble-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
-
-export default HomeRootLayout;
